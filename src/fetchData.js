6092446211c8a5b49f18f217a984bb229ab37438
gabriel <gabds.123@gmail.com>
2025-04-06 10:11:24 +0200

    permet d'aller chercher les donnes meteo

---
+++ src/fetchData.js
@@ -0,0 +1,5 @@
+export async function fetchData(location){
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=634DUWD4MYPK43R9SP3L552JF&contentType=json`);
+    const weatherData = await response.json();
+    console.log(weatherData);
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./style.css";
+import { fetchData } from "./fetchData";
+
+fetchData("Paris");
